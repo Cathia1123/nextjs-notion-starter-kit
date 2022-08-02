52060d77bbd1568e26ac051cb954b77478018506
Cathia1123 <106028145+Cathia1123@users.noreply.github.com>
2022-08-02 15:49:45 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: '',
   github: 'Cathia1123',
-  linkedin: 'https://www.linkedin.cn/incareer/in/chuyuehu',
+  linkedin: 'chuyuehu',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
